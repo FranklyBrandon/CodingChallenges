e17e9232177914caccb75c55eb95a128b5443558
FranklyBrandon <bran.frankenfield@gmail.com>
2021-12-28 15:37:04 -0600

    Start longest substring problem

--- Leetcode/MiddleOfTheLinkedList/MiddleOfTheLinkedList.py
+++ Leetcode/MiddleOfTheLinkedList/MiddleOfTheLinkedList.py
@@ -16,18 +16,21 @@ class Solution:
             count += 1
             current = current.next
         
-        place = (count / 2)
-        # Select the second middle if there are two middle nodes
-        if (place.is_integer()):
-            place += 1
-        # Otherwise round up to find the middle node split between two equal number of nodes
-        else: 
-            place = int(math.ceil(place))
+        if (count > 1):
+            place = (count / 2)
+            # Select the second middle if there are two middle nodes
+            if (place.is_integer()):
+                place += 1
+            # Otherwise round up to find the middle node split between two equal number of nodes
+            else: 
+                place = int(math.ceil(place))
+        else:
+            return head
             
         # Traverse nodes again to return the calculated middle node
         current = head
         count = 1
-        while(current.next):
+        while(current):
             if (count == place):
                 return current
             else:
